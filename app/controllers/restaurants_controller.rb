5c811cfe82f62c5b548308d0d2d9add91fb782eb
huier <andychen522.ambit@gmail.com>
2018-06-22 13:16:01 +0800

    add login process and update view with login messages

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,6 @@
 class RestaurantsController < ApplicationController
+    before_action :authenticate_user!
+    # 想要修改或美化，可以至 app/views/devise/sessions/new.html.erb 進行編輯
     def index
     end
 end
