d711f882cf3bab70bc9f3008efdd37c7e569789a
huier <andychen522.ambit@gmail.com>
2018-06-23 00:22:31 +0800

    set up admin authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
+  # application_controller.rb 是所有 controller 都可以取用的地方。即所有 controller 的 super class
+  
   protect_from_forgery with: :exception
+
+  private
+
+  def authenticate_admin
+    unless current_user.admin?
+      flash[:alert] = "Not allow!"
+      redirect_to root_path
+    end
+  end
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,7 @@
 class RestaurantsController < ApplicationController
     before_action :authenticate_user!
     # 想要修改或美化，可以至 app/views/devise/sessions/new.html.erb 進行編輯
+    
     def index
     end
 end
