7feb06dfdae88b476e725b93cb7c11612a51bc92
huier <andychen522.ambit@gmail.com>
2018-06-24 15:42:53 +0800

    add restaurant new and create actions and views for admin

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -5,5 +5,28 @@ class Admin::RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
-  
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      flash[:notice] = "restaurant was successfully created"
+      redirect_to admin_restaurants_path
+    else
+      flash.now[:alart] = "restaurant was failed to create"
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :tel, :address, :opening_hours, :description)
+    # 使用 params 取出從 Client 端送進的 request 裡 controller 所需參數，要求(require) restaurant 
+    # 拿出的表單資料必須使用 permit 允許指定的屬性資料傳進 Model
+  end
+
 end
