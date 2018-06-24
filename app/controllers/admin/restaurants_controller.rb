e34968a704abc03e5e24cf2d151d3bb11ca0bd66
huier <andychen522.ambit@gmail.com>
2018-06-24 22:34:46 +0800

    add restaurant edit and update actions and views for admin

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,6 +1,7 @@
 class Admin::RestaurantsController < ApplicationController
   before_action :authenticate_user!
   before_action :authenticate_admin
+  before_action :set_restaurant, only: [:show, :edit, :update]
 
   def index
     @restaurants = Restaurant.all
@@ -15,14 +16,22 @@ class Admin::RestaurantsController < ApplicationController
     if @restaurant.save
       flash[:notice] = "restaurant was successfully created"
       redirect_to admin_restaurants_path
+      # 新增完成後，回後台首頁
     else
       flash.now[:alart] = "restaurant was failed to create"
       render :new
     end
   end
 
-  def show
-    @restaurant = Restaurant.find(params[:id])
+  def update
+    if @restaurant.update(restaurant_params)
+      flash[:notice] = "restaurant was successfully updated"
+      redirect_to admin_restaurants_path(@restaurant)
+      # 修改完成後，回到原展示頁
+    else
+      flash.now[:alart] = "restaurant was failed to update"
+      render :edit
+    end
   end
 
   private
@@ -31,6 +40,10 @@ class Admin::RestaurantsController < ApplicationController
     params.require(:restaurant).permit(:name, :tel, :address, :opening_hours, :description)
     # 使用 params 取出從 Client 端送進的 request 裡 controller 所需參數，要求(require) restaurant 
     # 拿出的表單資料必須使用 permit 允許指定的屬性資料傳進 Model
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 
 end
