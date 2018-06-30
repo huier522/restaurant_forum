3220293cf7147d760ecc808151b7367a2a1422dd
huier <andychen522.ambit@gmail.com>
2018-06-30 18:49:57 +0800

    user can delete category with link on index page

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ApplicationRecord
   validates_presence_of :name
+  validates_uniqueness_of :name
+  # 確定新增餐廳分類名稱不會重複現有分類名稱
   
-  has_many :restaurants, dependent: :destroy
+  has_many :restaurants
 end
