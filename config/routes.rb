56521d8c8f8d9b326c596fc9f25ebc3dfa4db7c4
huier <andychen522.ambit@gmail.com>
2018-07-02 00:44:58 +0800

    setup restaurant resources for public users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :show]
+  # except 語意與 only 相反
+  # root "restaurants#index", except: [:new, :create, :edit, :update, 
+  # :destroy]
+  
   root "restaurants#index"
 
   namespace :admin do
