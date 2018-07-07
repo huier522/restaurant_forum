aab4bda39875714c7fafe61bb1066f18ce1930c4
huier <andychen522.ambit@gmail.com>
2018-07-07 12:15:04 +0800

    setup comment resources for public users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :restaurants, only: [:index, :show]
+  resources :restaurants, only: [:index, :show] do
+    resources :comments, only: [:create, :destroy]
+    # 利用 nested resources 的方式宣告特定餐廳下的評論，讓使用者可以 create，管理者可以 destroy
+  end
   # except 語意與 only 相反
   # root "restaurants#index", except: [:new, :create, :edit, :update, 
   # :destroy]
