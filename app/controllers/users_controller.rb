dd765784c995787ce0cd14681482e76bf1435a9e
huier <andychen522.ambit@gmail.com>
2018-07-12 02:00:42 +0800

    user can update the profile details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,33 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
 
   def show
+  end
+
+  def edit
+    unless @user == current_user
+      redirect_to user_path(@user)
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Profile was successfully updated"
+      redirect_to user_path(@user)
+    else
+      flash[:alarm] = "Profile was failed to update"
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :intro)
   end
   
 end
