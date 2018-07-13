edd3a3ff798b461fbe7573ebab9da0308f12b1e1
huier <andychen522.ambit@gmail.com>
2018-07-13 16:00:21 +0800

    user can view commented restaurants on profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   mount_uploader :avatar, AvatarUploader
-  
+
+  # 設定成 user 與 restaurant 多對多關聯，rails就會知道comments table
+  # 扮演了 join table的角色
   has_many :comments
+  has_many :restaurants, through: :comments
+
   
+  # admin? 讓我們用來判斷單個user是否有 admin 角色，列如：current_user.admin?
   def admin?
     self.role == "admin"
   end
