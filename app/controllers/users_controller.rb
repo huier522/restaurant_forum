34eaaee25f7f5951003f50b53a1ddc64ad3b7056
huier <andychen522.ambit@gmail.com>
2018-07-14 00:53:43 +0800

    remove duplicates from @user.restaurants

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
 
   def show
-    @commented_restaurants = @user.restaurants
+    # .uniq 使得 @user.restaurants 資料不重覆
+    @commented_restaurants = @user.restaurants.uniq
   end
 
   def edit
