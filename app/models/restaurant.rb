f2c6621c5f01fd3f6250fc47d4d0e8f1c1a6e176
huier <andychen522.ambit@gmail.com>
2018-07-16 23:55:17 +0800

    setup relation among user, restaurant and favorite

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -8,4 +8,8 @@ class Restaurant < ApplicationRecord
   # 加上 dependent: :destroy
   # 刪除 Restaurant 時，就可以一併刪掉關聯的評論
   has_many :comments, dependent: :destroy
+
+  # 餐廳擁有很多收藏者
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,10 @@ class User < ApplicationRecord
   # 扮演了 join table的角色
   has_many :restaurants, through: :comments
 
-  
+  # 「使用者收藏很多餐廳」的多對多關聯
+  has_many :favorites, dependent: :restrict_with_error
+  has_many :favorited_restaurants, through: :favorites, source: :restaurant
+
   # admin? 讓我們用來判斷單個user是否有 admin 角色，列如：current_user.admin?
   def admin?
     self.role == "admin"
