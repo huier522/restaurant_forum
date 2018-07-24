fb319364d4dfdbbd5cd9b9d2e4cde5aa354cf5ee
huier <andychen522.ambit@gmail.com>
2018-07-25 00:05:46 +0800

    validate uniqueness of friend_id in user_id scope

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,6 +1,11 @@
 class Friendship < ApplicationRecord
+
+  # 每個 user 都只能跟另一個 user 加一次好友 
+  validates :friend_id, uniqueness: { scope: :user_id }
+
   # 每一筆好友記錄，屬於發出交友的使用者(user)
   belongs_to :user
   # 每一筆好友記錄，屬於被加好友的使用者(friending)
   belongs_to :friend, class_name: "User"
+  
 end
